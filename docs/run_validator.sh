99ef36889b925336ae61d3a2dd735b1976b37732
flipchan <flipchan@users.noreply.github.com>
2022-11-15 12:46:23 +0100

    Create run_validator.sh

---
+++ docs/run_validator.sh
@@ -0,0 +1,12 @@
+
+compile the node with cargo:
+
+```
+git clone https://github.com/liberland/liberland_substrate && cd liberland_substrate && cargo build --release
+
+```
+
+install subkey:
+```
+cargo install subkey
+```
